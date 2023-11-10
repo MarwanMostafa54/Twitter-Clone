018109c7029ca3813fb1f670346e1d1ce56f58b0
marwan <marwanmostafa20002000@gmail.com>
2023-11-10 20:02:49 +0200

    1

--- public/js/common.js
+++ public/js/common.js
@@ -30,7 +30,7 @@ $("#postButton").click((event) => {
 
 function createpost(postData) {
   const user = postData.postedBy;
-  var timestamps = postData.createdAt;
+  var timestamp = timeDifference(new date(), new date(postData.createdAt));
   const postHTML = `
   <div class="post">
   <div class="mainContentContainer">
@@ -41,7 +41,7 @@ function createpost(postData) {
           <div class="header">        
           <a href="/profile/${user.username}" class="displayName">${user.firstName} ${user.lastName}</a>
           <span class="username">@${user.username}</span>
-          <span class="date"> ${timestamps}</span>
+          <span class="date"> ${timestamp}</span>
          </div>
         <div class="postBody">
           <span>${postData.content}</span>
@@ -68,4 +68,28 @@ function createpost(postData) {
         </div>
     `;
   return postHTML;
+}
+function timeDifference(current, previous) {
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000) return "just now";
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
 }
