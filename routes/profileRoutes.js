c07ab7e68d60e877edd1e43b4f86d68cb9fa0d90
marwan <marwanmostafa20002000@gmail.com>
2023-11-29 00:39:48 +0200

    1

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const User = require("../schemas/userSchema");
 const session = require("session");
 const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res, next) => {
   var payload = {
@@ -16,4 +17,29 @@ router.get("/", (req, res, next) => {
   };
   res.status(200).render("profilePage", payload);
 });
+
+router.get("/:usernameOrId", async (req, res, next) => {
+  const { usernameOrId } = req.params;
+  try {
+    let payload = {
+      pageTitle: "User not found",
+      userLoggedIn: req.session.user,
+      userLoggedInJs: JSON.stringify(req.session.user),
+      profileUser: null,
+    };
+    if (mongoose.Types.ObjectId.isValid(usernameOrId)) {
+      payload.profileUser = await User.findById(usernameOrId);
+    } else {
+      payload.profileUser = await User.findOne({ username: usernameOrId });
+    }
+    if (payload.profileUser) {
+      payload.pageTitle = payload.profileUser.username;
+    }
+    return res.status(200).render("profilePage", payload);
+  } catch (error) {
+    console.error("Error fetching user:", error.message);
+    return res.status(500).send(`Error fetching user data: ${error.message}`);
+  }
+});
+
 module.exports = router;
